e537f1157c19b66f68470cc56cc050a241eadc25
Qutiepatutie <ralphandre8@gmail.com>
2026-05-14 00:27:35 +0800

    Change embed output

--- bot.py
+++ bot.py
@@ -1,58 +1,3 @@
-""" import discord
-from discord.ext import commands
-import logging
-from dotenv import load_dotenv
-import os
-
-from config import REPO_FILE
-
-load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
-
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
-
-bot = commands.Bot(command_prefix="$", intents=intents)
-
-def save_repo(repo):
-    with open(REPO_FILE, "w") as f:
-        f.write(repo)
-
-def load_repo():
-    if not os.path.exists(REPO_FILE):
-        return None
-    
-    with open(REPO_FILE, "r") as f:
-        return f.read().strip()
-    
-def remove_repo():
-    if os.path.exists(REPO_FILE):
-        os.remove(REPO_FILE)
-
-@bot.command()
-async def connect(ctx, repo: str):
-    save_repo(repo)
-    await ctx.send(f"Connected to {repo.split('/')[-1]}")
-
-
-@bot.command()
-async def disconnect(ctx):
-    remove_repo()
-    await ctx.send(f"Disconnected to current repository")
-
-@bot.command()
-async def status(ctx):
-    repo = load_repo()
-
-    if repo:
-        await ctx.send(f"Watching: {repo.split('/')[-1]}")
-    else:
-        await ctx.send("Not watching any repository.")
-
-bot.run(token)
- """
 import os
 import hmac
 import hashlib
@@ -119,7 +64,7 @@ async def disconnect(ctx):
 async def status(ctx):
     repo = load_repo()
     if repo:
-        await ctx.send(f"Watching: {repo}")
+        await ctx.send(f"Watching: {repo.split('/')[-1]}")
     else:
         await ctx.send("Not watching any repository.")
 
@@ -176,7 +121,7 @@ async def github_webhook(request):
             lines.append(f"[`{sha}`]({url}) {msg}")
 
         embed = discord.Embed(
-            title=f"📦 New Push to {full_repo}",
+            title=f"📦 New Push to {full_repo.split('/')[-1]}",
             description="\n".join(lines),
             color=discord.Color.green()
         )
